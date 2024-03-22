158fe8328f8814b527aa6145623be8672adf052b
Aan Suseno <Aan Suseno>
2024-03-22 19:32:11 +0700

    fin

--- index.js
+++ index.js
@@ -24,6 +24,20 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/whoami', (req, res) => {
+  const ipAddress = req.headers['x-forwarded-for'];
+  const language = req.headers['accept-language'];
+  const software = req.headers['user-agent'];
+
+  const result = {
+    ipAddress,
+    language,
+    software
+  };
+
+  res.json(result);
+})
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
